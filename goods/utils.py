d3c40983d9b57b3a1e780aeb801b287d1f632adb
kategt <kotata546@gmail.com>
2024-04-19 14:26:52 -0400

    Full text search well-done

--- goods/utils.py
+++ goods/utils.py
@@ -1,9 +1,25 @@
-from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, SearchHeadline
 from django.db.models import Q
-
 
 from goods.models import Products
 
+
+# def q_search(query):
+#     if query.isdigit() and len(query) <= 5:
+#         return Products.objects.filter(id=int(query))
+#
+#     vector = SearchVector("name", "description")
+#     query = SearchQuery(query)
+#
+#     result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+#     result = result.annotate(
+#         headline=SearchHeadline("name", query, start_sel='<span style="background - color: yellow;">',
+#                                 stop_sel="</ span >", ))
+#
+#     result = result.annotate(
+#         bodyline=SearchHeadline("description", query, start_sel='<span style="background - color: yellow;">',
+#                                 stop_sel="</ span >", ))
+#     return result
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
@@ -12,12 +28,34 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
-   # keyword=[word for word in query.split() if len(word)>2]
-   # q_objects=Q()
-   # for token in keyword:
-   #     q_objects |= Q(description__icontains=token)
-   #     q_objects |= Q(name__icontains=token)
-   #
-   # return Products.objects.filter(q_objects)
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
+
+# keyword=[word for word in query.split() if len(word)>2]
+# q_objects=Q()
+# for token in keyword:
+#     q_objects |= Q(description__icontains=token)
+#     q_objects |= Q(name__icontains=token)
+#
+# return Products.objects.filter(q_objects)
